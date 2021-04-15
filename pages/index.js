c7188a4e9bde107de24d70e3c8142eed75cc7f6e
Samarth <codewithsamarth@gmail.com>
2021-04-16 01:04:12 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
-import {Text , Box , Container , Heading,Card,Image ,Button,Gradient,Flex,Link} from 'theme-ui'
-import base from '@hackclub/theme'
+import Workflow from '../components/Workflow/index';
 function Home() {
     return (
-      <Container variant='wide' >
-        <Box backgroundColor= 'steel' p = '15px'>
-        <Flex>
-          <Image src = '/logo.png' width='150px' height='56px' />
-          
-          <Heading  as="h1" variant ='ultratitle'> ---  </Heading>
-        <Box ml = 'auto' mt='auto' mb ='auto'>
-          <Button margin = '5px'>BTN 1</Button>
-          <Button margin = '5px'>BTN 2</Button>
-          <Button margin = '5px'>BTN 3 q </Button>
-        </Box>
-        </Flex>
-        </Box>
-
-        <Box>
-          
-          <Heading as= 'h2' variant='title' a >How to Get Started </Heading>
-
-          </Box>  
-         
-          
-        </Container>
+     <Workflow/>
           
     )
   }
