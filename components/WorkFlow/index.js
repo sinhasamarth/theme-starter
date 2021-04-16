9541f1fa3e3fb8e98a76f7ee4094fb50e0d168ec
Samarth <codewithsamarth@gmail.com>
2021-04-16 11:31:19 +0530

    Juding Done

--- components/WorkFlow/index.js
+++ components/WorkFlow/index.js
@@ -1,35 +1,38 @@
-    import {Box, Container, Heading,Flex} from 'theme-ui'
+    import {Box, Container, Heading,Flex,Card} from 'theme-ui'
     function Home() {
         return (
         
             <Container variant ='container' >
+                
                 <Heading as= 'h2' variant='title' a >How to Get Started </Heading> 
 
+                <Card variant="primary">
                 <Flex m= '5em' background= '/logo.png'>
-                    <img src = "/register.gif" width='200em'/>
+                    <img src = "/register.gif"  width='150em' height='150em' style = {{  borderRadius: '99999px'}}/>
                     <Box padding='2em'>
-                    <Heading  variant='title'>Step 1 </Heading>
+                    <Heading  variant='title'  >Step 1 </Heading>
                         <Heading  variant='headline'>Register Your Self</Heading>
                     </Box>
                 </Flex>
 
 
                 <Flex m= '5em'>
-                    <img src = "/to-do.gif" width='200em'/>
+                    <img src = "/to-do.gif"
+                     width='150em' height='150em'  width='150em' height='150em' style = {{  borderRadius: '99999px'}}/>
                     <Box padding='2em'>
                     <Heading  variant='title'>Step 2 </Heading>
                         <Heading  variant='headline'>Short List </Heading>
                     </Box>
                 </Flex>
                 <Flex m= '5em'>
-                    <img src = "/register.gif" width='200em'/>
+                    <img src = "/register.gif" width='150em' height='150em' style = {{  borderRadius: '99999px'}}/>
                     <Box padding='2em'>
                     <Heading  variant='title'>Step 3 </Heading>
                         <Heading  variant='headline'>Join Our Channels</Heading>
                     </Box>
                 </Flex>
                 <Flex m= '5em'>
-                    <img src = "/trophy-animation.gif" width='200em'/>
+                    <img src = "/trophy-animation.gif" width='150em' height='150em' style = {{  borderRadius: '99999em'}}/>
                     <Box padding='2em'>
                     <Heading  variant='title'>Step 5 </Heading>
                         <Heading  variant='headline'>Join Our Channels</Heading>
@@ -37,7 +40,7 @@
                 </Flex>
                 
 
-                
+                </Card>
             </Container>
             
         
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Workflow from '../components/Workflow/index';
+import Juding from '../components/Juding/index';
+import FAQ from '../components/FAQ/index';
+import {Images} from 'theme-ui'
 function Home() {
     return (
-     <Workflow/>
-          
+      <>
+      <Juding/>
+      
+      </>
     )
   }
   
